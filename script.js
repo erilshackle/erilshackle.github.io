fc2a258a22f7056c0ea0768bd79e8317b9c2048d
Eril Shackle <74083037+erilshackle@users.noreply.github.com>
2021-08-08 00:16:10 -0100

    menu

---
+++ script.js
@@ -0,0 +1,13 @@
+$(".menu-ham").click(function(){
+    $(".sidebar").toggleClass("show-menu");
+    $(".menu-ham").toggleClass("show-menu");
+})
+
+// const menu_ham = document.querySelector(".menu-ham");
+
+// menu_ham.addEventListener('click', function(){
+//     var sidebar = document.querySelector(".sidebar");
+//     var menu_ham = document.querySelector(".menu-ham")
+//     sidebar.classList.toggle("show-menu");
+//     menu_ham.classList.toggle("show-menu");
+// });
