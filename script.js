280ee9e019b67de227f4b517a25f517d9445094e
Eril Shackle <74083037+erilshackle@users.noreply.github.com>
2021-08-09 21:35:15 -0100

    update

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+
 $(".menu-ham").click(function(){
     $(".sidebar").toggleClass("show-menu");
     $(".menu-ham").toggleClass("show-menu");
 })
+
+function closeMenu(){
+    let sidebar = document.getElementsByClassName('sidebar');
+    let ham = document.getElementsByClassName('menu-ham');
+    sidebar[0].classList.toggle('show-menu');
+    ham[0].classList.toggle('show-menu');
+}
+
 
 // const menu_ham = document.querySelector(".menu-ham");
 
